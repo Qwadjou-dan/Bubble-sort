c99918b86eda356b950bda7e2dc973ae772aeaf1
owusudaniel077@gmail.com <qwadjou@Daniels-MacBook-Pro.local>
2024-09-27 06:11:42 +0000

    Sorting out Numbers-JS

---
+++ scripts/bubble-sort.js
@@ -0,0 +1,21 @@
+
+// const nums = [5, 6, 2, 1, 9, 4, 3, 8, 7, 0];
+
+let bubbleSort = (array) => {
+  for(let i = 0; i < array.length; i++){
+    for(let x = 0; x < array.length - 1; x++){
+      if(array[x] > array[x+1]){
+        let y = array[x];
+        array[x] = array[x+1];
+        array[x+1] = y;
+      };
+    };
+  };
+  return array;
+};
+
+console.log(bubbleSort([5, 6, 2, 1, 9, 4, 3, 8, 7, 0]));
+
+console.log(bubbleSort([5, 6, 3, 8, 7, 0]));
+
+
