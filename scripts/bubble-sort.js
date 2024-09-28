47c5979de19c53002bf392d82575e4ee50f17ead
owusudaniel077@gmail.com <qwadjou@Daniels-MacBook-Pro.local>
2024-09-28 14:30:57 +0000

    User expected to Input values

--- scripts/bubble-sort.js
+++ scripts/bubble-sort.js
@@ -1,23 +1,66 @@
 
-let nums = [5, 6, 2, 1, 9, 4, 3, 8, 7, 0];
 
-let bubbleSort = (array) => {
-  for(let i = 0; i < array.length; i++){
-    for(let x = 0; x < array.length - 1; x++){
-      if(array[x] > array[x+1]){
-        let y = array[x];
-        array[x] = array[x+1];
-        array[x+1] = y;
+// Converting inputted value into an Array
+
+document.querySelector('.js-done-button').addEventListener('click', () => {
+
+  let input = document.querySelector('.js-number-input').value;
+  nums = input.split(',').map(Number);
+
+  // Generating the HTML
+  let barsHTML = '';
+
+  nums.forEach((num) => {
+
+    barsHTML += `
+      <div class="bar-design">${num}</div>
+    `
+  })
+
+  document.querySelector('.js-bar')
+    .innerHTML = barsHTML;
+  
+})
+
+
+// Sorting function (Bubble Sort)
+
+  let bubbleSort = (array) => {
+    for(let i = 0; i < array.length; i++){
+      for(let x = 0; x < array.length - 1; x++){
+        if(array[x] > array[x+1]){
+          let y = array[x];
+          array[x] = array[x+1];
+          array[x+1] = y;
+        };
       };
     };
+    return array;
   };
-  return array;
-};
-
-// console.log(bubbleSort(nums));
 
 
+// Sorted Bars Display on Button Click
+
+ document.querySelector('.js-sort-button')
+  .addEventListener('click', () => {
+
+    let sortedNums = bubbleSort([...nums]);
 
 
+// Generating the HTML
+    let sortedBarsHTML = '';
 
+    sortedNums.forEach((num) => {
+
+      sortedBarsHTML += `
+        <div class="bar-design">${num}</div>
+      `
+    })
+
+    document.querySelector('.js-bar')
+      .innerHTML = sortedBarsHTML;
+
+    });
+
+    let nums = [];
 
